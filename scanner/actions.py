f714421961ef3f2ca1ef5b75e7569e8082ed9757
Mark Joslin <mjosliN@gmail.com>
2011-08-02 14:03:53 -0400

    ...

--- scanner/actions.py
+++ scanner/actions.py
@@ -18,64 +18,14 @@ TODO
 #INDENTATION
 indentStack = [1]
 
-def grabCol(s,l):
-	return col(l,s)
-	
-def checkIndent(s,l,t):
-	#Grab current column
-	currentCol = grabCol(s,l)
-	#print currentCol
-	
-	#The indent stack & indent level
-	global indentStack, indentLevel
-	if(not indentStack):
-		indentStack = [1]
-		
-	if(currentCol <= indentStack[-1]):
-		raise ParseException(s,l,"Not an indent")
-		
-	#If the column is greater than the previous line's column
-	indentStack.append(currentCol)
-	indentLevel += 1
-
-def checkUndent(s,l,t):
-	#Grab current column
-	currentCol = grabCol(s,l)
-	#print currentCol
-	
-	global indentStack, indentLevel
-	changed = False
-	while(currentCol < indentStack[-1] and len(indentStack) > 1):
-		indentLevel -= 1
-		indentStack.pop()
-		changed = True
-	
-	if(not changed):
-		raise ParseException(s,l,"Not an undent")
-
-def checkSamedent(s,l,t):
-	currentCol = grabCol(s,l)
-	print currentCol
-	
-	global indentStack
-	if(not indentStack):
-		pass
-	elif(not currentCol == indentStack[-1]):
-		raise ParseException(s,l,"Not same dent")
-
-def checkReservedWords(s,l,t):
-	reserves = ['else','if','elif','else if']
-	print "ATOM CHECKING.." + t[0]
-	if t[0] in reserves:
-		raise ParseException(s,l,"Part of reserved words")
 def getCol(s,l,t):
 		print l
 		print col(l,s)
 		print s	
+
 #SYMBOL TABLE
 symbol_table = []
-INDENT = lineStart.setParseAction(checkIndent).setDebug().setName('indent')
-UNDENT = lineEnd.suppress() + empty + empty.copy().setParseAction(checkUndent).setDebug().setName('undent')
+
 '''Class Structure
 
 					[stmt]
@@ -159,7 +109,7 @@ class IfStatement(CompoundStatement):
 		pprint.pprint(t.asList())
 		self.tokens = t.asList()
 	def __str__(self):
-		return pprint.pformat(self.t.asList())
+		return pprint.pformat(self.t.asList()[0][1].getOperator())
 	def accept(self):
 		for t in self.tokens:
 			try:
@@ -219,5 +169,48 @@ class Expression(SmallStatement):
 			try:
 				t.visit()
 			except:
-				pass #Do a literal translate here?
+				pass
+class Comparison(Expression):
+	def __init__(self, t):
+		self.t = t
+		self.tokens = t.asList()
+		#pprint.pprint(t.asList())
+	def __str__(self):
+		return pprint.pformat(self.t.asList())
+	def asList(self):
+		return self.tokens
+	def getOperator(self):
+		return type(self.tokens[1])
+	def accept(self):
+		for t in self.tokens:
+			try:
+				t.visit()
+			except:
+				pass
+class Name(Expression):
+	def __init__(self, t):
+		self.t = t
+		self.tokens = t.asList()
+		#pprint.pprint(t.asList())
+	def __str__(self):
+		return pprint.pformat(self.t.asList())
+	def accept(self):
+		for t in self.tokens:
+			try:
+				t.visit()
+			except:
+				pass
+class Name(Expression):
+	def __init__(self, t):
+		self.t = t
+		self.tokens = t.asList()
+		#pprint.pprint(t.asList())
+	def __str__(self):
+		return pprint.pformat(self.t.asList())
+	def accept(self):
+		for t in self.tokens:
+			try:
+				t.visit()
+			except:
+				pass
 		
