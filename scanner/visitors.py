bcfb9e7ae120dcf1ff94e553497a7a98d57cd52d
Mark Joslin <mjosliN@gmail.com>
2011-08-05 15:31:01 -0400

    Successfully concatenates if statements into one with brackets and shit

--- scanner/visitors.py
+++ scanner/visitors.py
@@ -23,16 +23,27 @@ class Visitor(object):
 		
 	#Going in reverse order coinicides with the stack
 	#Fills in any non-str positions from right-left
-	def merge(self,element):
+	def merge(self,element, debug=False):
 		l = range(len(element))
 		l.reverse()
 		for x in l:
 			if type(element[x]) != type(''):
-				element[x] = self.tokens.pop()
+				a = self.tokens.pop()
+				if debug:
+					print "Popping " + a
+					raw_input()
+				element[x] = a
 		r = ' '.join(element)
 		self.tokens.append(r)
 		print self.tokens
-		
+	def append(self, trailer):
+		x = self.tokens.pop()
+		x += trailer
+		self.tokens.append(x)
+	def prepend(self, pre):
+		x = self.tokens.pop()
+		x = pre + x
+		self.tokens.append(x)
 	
 	#[ROOT]
 	def visit_Root(self, element): 
@@ -138,68 +149,9 @@ class Visitor(object):
 	def visit_Number(self, element):
 		pass
 
-translate_dict = {'print':'printf({0})'}
-
-class PrintVisitor(object):
-	#Calls function from its string name
-	def _func(self,name):
-		f = getattr(PrintVisitor(), 'visit_' + name)
-		return f
-	#General dispatcher
-	def visit(self,*args):
-		name = type(args[0]).__name__
-		elem = args[0]
-		visit_func = self._func(type(elem).__name__)
-		visit_func(elem)
-	
-	#Visit methods
-	def visit_Root(self, element):
-		print str(element)
-	def visit_Statement(self,element):
-		print str(element)
-	def visit_CompoundStatement(self,element):
-		print str(element)
-	def visit_SimpleStatement(self,element):
-		print str(element)
-	def visit_SmallStatement(self,element):
-		print str(element)
-	def visit_Suite(self, element):
-		print str(element)
-	def visit_IfStatement(self, element):
-		print str(element)
-	def visit_ForStatement(self, element):
-		print str(element)
-	def visit_WhileStatement(self, element):
-		print str(element)
-	def visit_FunctionDeclaration(self, element):
-		print str(element)
-	def visit_Expression(self, element):
-		print str(element)
-	def visit_Comparison(self, element):
-		print str(element)
-	def visit_Name(self, element):
-		print str(element)
-	def visit_String(self, element):
-		print str(element)
-	def visit_str(self,element):
-		print element
-
 class PrintListVisitor(Visitor):
 	import actions
 	
-	def __init__(self):
-		self.tokens = []
-	''''	
-	#Calls function from its string name
-	def _func(self,name):
-		f = getattr(self, 'visit_' + name)
-		return f
-	def visit(self,*args):
-		name = type(args[0]).__name__
-		elem = args[0]
-		visit_func = self._func(type(elem).__name__)
-		visit_func(elem)
-	'''
 	#Visit methods
 	def visit_Root(self, element):
 		print type(element).__name__
@@ -221,14 +173,23 @@ class PrintListVisitor(Visitor):
 		print type(element).__name__
 		print element
 		raw_input()
+		self.append(';\n')
+		
 	def visit_Suite(self, element):
 		print type(element).__name__
 		print element
+		print self.tokens
 		raw_input()
+		self.merge(element)
+		self.prepend('{\n')
+		self.append('\n}')
+		
 	def visit_IfStatement(self, element):
 		print type(element).__name__
 		print element
 		raw_input()
+		self.merge(element, debug=True)
+		
 	def visit_ForStatement(self, element):
 		#print type(element).__name__
 		pass
